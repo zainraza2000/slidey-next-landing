76e577de3bcc9423eecb21f4a0bd18d07f399325
Muhammad Zain Raza <zainraza20000@gmail.com>
2024-02-19 17:53:25 +0500

    fixes

--- components/shared/ImageModal.jsx
+++ components/shared/ImageModal.jsx
@@ -37,7 +37,7 @@ const ImageModal = ({ img, open, setOpen, cancelButtonRef, customClass }) => {
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel
-                className={`relative transform overflow-hidden  bg-white text-left shadow-xl transition-all ${customClass}`}
+                className={`relative transform overflow-hidden  bg-white text-left shadow-xl transition-all ${customClass} rounded-lg`}
               >
                 <div className="relative p-0">
                   <IoClose
