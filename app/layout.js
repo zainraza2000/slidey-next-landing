a147621f1ad80fb9c8d70df71120c18a54f6e8ac
zainraza2000 <zainraza20000@gmail.com>
2024-05-22 19:18:20 +0500

    modify text

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Slidey.io",
-  description: "Designing Decks, Pitch Perfectly!",
+  description: "Your Design Partner!",
 };
 
 export default function RootLayout({ children }) {
